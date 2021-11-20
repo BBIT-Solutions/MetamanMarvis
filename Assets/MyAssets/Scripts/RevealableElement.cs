9c6752e6a17cfb27a2e85231c6f4d613b73be762
bennibayer <benni_bayer@web.de>
2021-11-20 17:20:38 +0100

    link all states of level 2

--- Assets/MyAssets/Scripts/RevealableElement.cs
+++ Assets/MyAssets/Scripts/RevealableElement.cs
@@ -10,8 +10,10 @@ public class RevealableElement : MonoBehaviour {
         LEVEL3_GrabableGunElement3, LEVEL3_GunHintElement3,
         LEVEL4_GrabableGunElement4, LEVEL4_GunHintElement4,
         LEVEL5_GrabableGunElement5, LEVEL5_GunHintElement5,
-        LEVEL2_xyzTODO,
-        LEVEL5FINAL____xyzTODO_enemy //something like this can you also assign to multiple gameObjects
+        HIDE_VIRTUAL_ELEMENTS, SHOW_VIRTUAL_ELEMENTS,
+        GLOVES_ON_CONTROLLER,
+        FULL_GUN, //is handled different (by quest/puzzle logic), but to have the option here too
+        ENEMY_TARGET //something like this can also be assigned to multiple gameObjects
     };
 
     [SerializeField] public RevealableElementTag revealableElementTag;
